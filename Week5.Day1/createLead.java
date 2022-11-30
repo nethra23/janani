6fa12995edbac7f5e1370519b55e7027de3b6d08
nethra23 <115528543+nethra23@users.noreply.github.com>
2022-12-01 00:21:29 +0530

    Add files via upload

---
+++ Week5.Day1/createLead.java
@@ -0,0 +1,20 @@
+package Week5.Day1;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+public class createLead extends BaseClass {
+	@Test
+	public void runCreateLead() {
+	
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+	
+}
+	
+
+}
